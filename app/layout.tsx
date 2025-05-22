59fe2e09106ea6ba0ece61860f36e88f319cab74
gabriel miranda <gabrielmfern@outlook.com>
2025-05-22 07:54:03 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter, Noto_Sans_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const notoSansMono = Noto_Sans_Mono({
+  variable: "--font-noto-sans-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Outlook Preview",
+  description:
+    "Get an approximate preview of how your email will look in Outlook",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${notoSansMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Highlight, themes } from 'prism-react-renderer';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col container mx-auto">
+      <p className="text-2xl text-slate-10">This implements a proof-of-concept for previewing </p>
+      <div className=""></div>
+    </main>
+  );
+}
