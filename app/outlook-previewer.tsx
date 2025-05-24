1e971b2a88eb19c87c324e2d47f12f4ca8f26b24
gabriel miranda <gabrielmfern@outlook.com>
2025-05-24 11:06:08 -0300

    center tabs, and keep them above right split

--- app/outlook-previewer.tsx
+++ app/outlook-previewer.tsx
@@ -75,85 +75,84 @@ export function OutlookPreviewer() {
   const lastDefinedRenderingResult = useLastDefinedValue(renderingResult);
 
   return (
-    <Tabs.Root
-      className="bg-slate-2 h-[90dvh] flex flex-col rounded-md border border-slate-8"
-      defaultValue="preview"
-    >
-      <Tabs.List className="w-full flex justify-end border-b border-solid border-b-slate-8">
-        <Tabs.Tab value="preview">Preview</Tabs.Tab>
-        <Tabs.Tab value="code">Code</Tabs.Tab>
-      </Tabs.List>
-      <SplitView className="h-full">
-        {[
-          (width) => (
-            <div style={{ width }} className="h-full">
-              <CodeEditor code={react} onChange={(v) => setReact(v)} />
-            </div>
-          ),
-          (width) => (
-            <div style={{ width }} className="relative h-full">
-              {renderingResult && "exception" in renderingResult ? (
-                <ErrorOverlay
-                  exception={renderingResult.exception}
-                  className="absolute left-1/2 top-1/2 -translate-1/2 w-2/3 h-1/3"
-                />
-              ) : null}
-              <div className="h-full">
-                <Tabs.Panel value="preview" className="w-full h-full">
-                  {lastDefinedRenderingResult &&
-                  "html" in lastDefinedRenderingResult ? (
-                    <iframe
-                      srcDoc={lastDefinedRenderingResult.html}
-                      ref={(iframe) => {
-                        if (iframe) {
-                          const mouseMoveBubbler = (event: MouseEvent) => {
-                            const bounds = iframe.getBoundingClientRect();
-                            document.dispatchEvent(
-                              new MouseEvent("mousemove", {
-                                ...event,
-                                clientX: event.clientX + bounds.x,
-                                clientY: event.clientY + bounds.y,
-                              }),
-                            );
-                          };
-                          const mouseUpBubbler = (event: MouseEvent) => {
-                            document.dispatchEvent(
-                              new MouseEvent("mouseup", event),
-                            );
-                          };
-                          iframe.contentDocument?.addEventListener(
+    <SplitView className="h-[90dvh] bg-slate-2 rounded-md border border-slate-8 overflow-visible">
+      {[
+        (width) => (
+          <div style={{ width }} className="h-full">
+            <CodeEditor code={react} onChange={(v) => setReact(v)} />
+          </div>
+        ),
+        (width) => (
+          <Tabs.Root
+            className="h-full flex flex-col relative"
+            style={{ width }}
+            defaultValue="preview"
+          >
+            <Tabs.List className="border-b border-slate-4 flex justify-center py-1">
+              <Tabs.Tab value="preview">Preview</Tabs.Tab>
+              <Tabs.Tab value="code">Code</Tabs.Tab>
+            </Tabs.List>
+            {renderingResult && "exception" in renderingResult ? (
+              <ErrorOverlay
+                exception={renderingResult.exception}
+                className="absolute left-1/2 top-1/2 -translate-1/2 w-2/3 h-1/3"
+              />
+            ) : null}
+            <div className="flex-grow">
+              <Tabs.Panel value="preview" className="w-full h-full">
+                {lastDefinedRenderingResult &&
+                "html" in lastDefinedRenderingResult ? (
+                  <iframe
+                    srcDoc={lastDefinedRenderingResult.html}
+                    ref={(iframe) => {
+                      if (iframe) {
+                        const mouseMoveBubbler = (event: MouseEvent) => {
+                          const bounds = iframe.getBoundingClientRect();
+                          document.dispatchEvent(
+                            new MouseEvent("mousemove", {
+                              ...event,
+                              clientX: event.clientX + bounds.x,
+                              clientY: event.clientY + bounds.y,
+                            }),
+                          );
+                        };
+                        const mouseUpBubbler = (event: MouseEvent) => {
+                          document.dispatchEvent(
+                            new MouseEvent("mouseup", event),
+                          );
+                        };
+                        iframe.contentDocument?.addEventListener(
+                          "mousemove",
+                          mouseMoveBubbler,
+                        );
+                        iframe.contentDocument?.addEventListener(
+                          "mouseup",
+                          mouseUpBubbler,
+                        );
+                        return () => {
+                          iframe.contentDocument?.removeEventListener(
                             "mousemove",
                             mouseMoveBubbler,
                           );
-                          iframe.contentDocument?.addEventListener(
+                          iframe.contentDocument?.removeEventListener(
                             "mouseup",
                             mouseUpBubbler,
                           );
-                          return () => {
-                            iframe.contentDocument?.removeEventListener(
-                              "mousemove",
-                              mouseMoveBubbler,
-                            );
-                            iframe.contentDocument?.removeEventListener(
-                              "mouseup",
-                              mouseUpBubbler,
-                            );
-                          };
-                        }
-                      }}
-                      className="w-full h-full bg-white"
-                      title="Outlook Preview"
-                    />
-                  ) : null}
-                </Tabs.Panel>
-                <Tabs.Panel className="w-full h-full" value="code">
-                  [Email preview HTML after having been transformed]
-                </Tabs.Panel>
-              </div>
+                        };
+                      }
+                    }}
+                    className="w-full h-full bg-white z-0"
+                    title="Outlook Preview"
+                  />
+                ) : null}
+              </Tabs.Panel>
+              <Tabs.Panel className="w-full h-full" value="code">
+                [Email preview HTML after having been transformed]
+              </Tabs.Panel>
             </div>
-          ),
-        ]}
-      </SplitView>
-    </Tabs.Root>
+          </Tabs.Root>
+        ),
+      ]}
+    </SplitView>
   );
 }
