84f55881606162f137223f9849d39d726682c9ee
gabriel miranda <gabrielmfern@outlook.com>
2025-05-24 09:49:10 -0300

    add top padding to home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { OutlookPreviewer } from "./outlook-previewer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col container mx-auto">
+    <main className="flex flex-col container mx-auto pt-15">
       <OutlookPreviewer />
     </main>
   );
